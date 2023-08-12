8c5c15d81101cab8d7dacee887b45d832ea248c8
AnnaKhiz <kukurika.anna@gmail.com>
2023-08-12 14:46:08 +0300

    header done

--- src/js/modules/address-menu.js
+++ src/js/modules/address-menu.js
@@ -2,12 +2,16 @@ export function callAddressMenu() {
     const addressBtn = document.getElementById('address-menu');
     const submenuHeader = document.getElementById('submenu-header');
     const closeMenuBtn = document.getElementById('close-menu');
-    addressBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        submenuHeader.classList.toggle('hidden')
-    });
-    closeMenuBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        submenuHeader.classList.toggle('hidden')
-    })
+    const contactsLink = document.querySelector('.header__container-contacts-link ')
+
+    function showHideMenu (button) {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            submenuHeader.classList.toggle('hidden');
+            contactsLink.classList.toggle('colored-line');
+        });
+    }
+
+    showHideMenu (addressBtn)
+    showHideMenu (closeMenuBtn)
 }
