ced3481a95a6811e0eed82af99794ce833047d58
AnnaKhiz <kukurika.anna@gmail.com>
2023-08-12 14:58:57 +0300

    header done+

--- src/js/modules/address-menu.js
+++ src/js/modules/address-menu.js
@@ -2,7 +2,7 @@ export function callAddressMenu() {
     const addressBtn = document.getElementById('address-menu');
     const submenuHeader = document.getElementById('submenu-header');
     const closeMenuBtn = document.getElementById('close-menu');
-    const contactsLink = document.querySelector('.header__container-contacts-link ')
+    const contactsLink = document.querySelector('.header__container-contacts-link');
 
     function showHideMenu (button) {
         button.addEventListener('click', (e) => {
