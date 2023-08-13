f26c24b0c6b3ff02f3406eb080ebfaa0a0cae644
AnnaKhiz <kukurika.anna@gmail.com>
2023-08-14 00:47:38 +0300

    video-block done

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as header from "./modules/address-menu.js";
+import * as video from "./modules/index-play.js";
 
 flsFunctions.isWebp();
 header.callAddressMenu();
+video.hideLayers();
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
