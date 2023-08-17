9c5626526e31864b0a4491fe08d44d7f87ed60f3
AnnaKhiz <kukurika.anna@gmail.com>
2023-08-18 00:04:24 +0300

    slider done

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as header from "./modules/address-menu.js";
 import * as video from "./modules/index-play.js";
+import * as swiper from "./modules/swiper.js";
 
 flsFunctions.isWebp();
 header.callAddressMenu();
 video.hideLayers();
+swiper.swipeIndexSwiper();
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
