c088bcb70677e56f87d9fcec4b13e54aa6c34bd6
AnnaKhiz <kukurika.anna@gmail.com>
2023-09-16 20:55:34 +0300

    header adaptive finished

--- src/js/modules/address-menu.js
+++ src/js/modules/address-menu.js
@@ -1,21 +1,14 @@
 export function callAddressMenu() {
     const addressBtn = [...document.querySelectorAll('[data-header-link="address"]')];
     const submenuHeader = [...document.querySelectorAll('[data-city-list="submenu-header"]')];
-    console.log(addressBtn)
-    console.log(submenuHeader)
     const closeLink = [...document.querySelectorAll('[data-close-link="header-sub-close"]')];
-    console.log(closeLink)
-    // const closeMenuBtn = [...document.querySelectorAll('close-menu')];
-    // const contactsLink = document.querySelector('.header__container-contacts-link');
 
     function showHideMenu (button) {
         button.addEventListener('click', (e) => {
             e.preventDefault();
             submenuHeader.forEach((e) => {
                 e.classList.toggle('hidden');
-            })
-            // submenuHeader.classList.toggle('hidden');
-            // contactsLink.classList.toggle('colored-line');
+            });
         });
     }
 
@@ -29,9 +22,4 @@ export function callAddressMenu() {
         showHideMenu(e)
     });
 
-    // submenuHeader.forEach((e) => {
-    //     showHideMenu (e)
-    // });
-    // showHideMenu (addressBtn)
-    // showHideMenu (closeMenuBtn)
 }
