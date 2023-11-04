4537198760e3c3bc9746333ecc309f5f15d7b570
AnnaKhiz <kukurika.anna@gmail.com>
2023-11-04 17:23:04 +0200

    adapt section "video"

--- src/js/modules/index-play.js
+++ src/js/modules/index-play.js
@@ -3,15 +3,24 @@ export function hideLayers() {
     const imageCover = document.getElementById('index-video-cover');
     const playBtn = document.getElementById('index-video-play');
     const video = document.getElementById('video');
+    let imageCoverHeight = 0;
 
     playBtn.addEventListener('click', (e) => {
-        console.log('click')
         e.preventDefault();
+        imageCoverHeight = imageCover.height;
+
         container.classList.remove('pseudo')
         imageCover.classList.add('hidden');
         playBtn.classList.add('hidden');
-
         video.src += '&autoplay=1';
+        video.style.visibility = 'initial';
+        video.style.position = 'static';
+
+        if (window.screen.width >= 768) {
+            video.style.height = '436px';
+        } else {
+            video.style.height = `${imageCoverHeight}px`;
+        }
 
     })
 }
