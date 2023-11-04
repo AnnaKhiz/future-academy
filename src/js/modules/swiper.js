a25dfce11b5ca0b5576e478dbc40338553f84159
AnnaKhiz <kukurika.anna@gmail.com>
2023-11-04 21:14:38 +0200

    started adaptive swiper slider

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -11,5 +11,31 @@ export function swipeIndexSwiper() {
         //     el: ".swiper-pagination",
         //     clickable: true,
         // },
+        breakpoints: {
+            300: {
+                slidesPerView: 1,
+                spaceBetween: 15
+            },
+            425: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            1130: {
+                slidesPerView: 4,
+                spaceBetween: 15
+            },
+            1280: {
+                slidesPerView: 5,
+                spaceBetween: 20
+            },
+            1601: {
+                slidesPerView: 6,
+                spaceBetween: 20
+            }
+        }
     });
 }
