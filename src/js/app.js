af8bfed82585ad5789f0fc9c8afca4d21f948ac1
AnnaKhiz <kukurika.anna@gmail.com>
2023-12-14 18:01:21 +0200

    add back button

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as back from "./modules/back.js";
 import * as header from "./modules/address-menu.js";
 import * as video from "./modules/index-play.js";
 import * as swiper from "./modules/swiper.js";
@@ -7,7 +8,7 @@ import * as range from './modules/range-slider.js';
 import * as aside from './modules/aside-menu.js';
 
 
-
+back.backToPortfolio();
 burger.callBurgerMenu();
 flsFunctions.isWebp();
 header.callAddressMenu();
---
+++ src/js/modules/back.js
@@ -0,0 +1,6 @@
+export function backToPortfolio() {
+  const backButton = document.getElementById('button-back');
+  backButton.addEventListener('click', () => {
+    location.replace('https://anna-khizhniak.site/')
+  })
+}
